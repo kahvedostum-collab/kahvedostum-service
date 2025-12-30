4724db69cf38ccda22975c557d87ded415dbfa60
ozcanblkn <97128072+ozcanblkn@users.noreply.github.com>
2025-12-30 03:14:31 +0300

    fix: avoid updating ReceiptHash on duplicate receipts (#39)
    Co-authored-by: ozcan <ozcan.balkanli@resolution7.com>

--- KahveDostum_Service/obj/Debug/net9.0/KahveDostum_Service.AssemblyInfo.cs
+++ KahveDostum_Service/obj/Debug/net9.0/KahveDostum_Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KahveDostum_Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634d9bfb6eaa8a172a8f54691b2890b64235bfc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79616a65c5f93c54fd073d434dbacc14931900b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("KahveDostum_Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KahveDostum_Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
