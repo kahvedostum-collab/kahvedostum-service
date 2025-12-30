01ce376cfb052a3e7d8af9ae86c346c3c725d744
ozcan <ozcan.balkanli@resolution7.com>
2025-12-30 23:48:08 +0300

    SQL Server güvenliği ve sürekliliği güçlendirildi
    - MSSQL Docker container güvenli şekilde yeniden kuruldu
    - Varsayılan 1433 portu kaldırılıp rastgele yüksek porta taşındı (49172)
    - Veritabanı kalıcılığı için /volume1/docker → /var/opt/mssql volume bağlantısı sağlandı
    - Yeni güçlü admin kullanıcısı oluşturuldu kdadmin, sa hesabı devre dışı bırakıldı
    - Fail2Ban kurulup MSSQL brute force saldırılarına karşı aktif hale getirildi
    - 2 kez yanlış denemede 24 saatlik ban kuralı getirildi.

--- KahveDostum_Service/obj/Debug/net9.0/KahveDostum_Service.AssemblyInfo.cs
+++ KahveDostum_Service/obj/Debug/net9.0/KahveDostum_Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KahveDostum_Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79616a65c5f93c54fd073d434dbacc14931900b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0402f125bb65d495789cfd5a29b20c59e041bae")]
 [assembly: System.Reflection.AssemblyProductAttribute("KahveDostum_Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KahveDostum_Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
